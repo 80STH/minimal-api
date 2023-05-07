c76f81d4b33ff9eeb5e636bd7da68f6669b114a6
STH <STH@DESKTOP-E40LHSD>
2023-05-07 18:22:09 +0600

    123

--- minimal api/Models/Datas.cs
+++ minimal api/Models/Datas.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Runtime.Serialization;
+
 
 namespace Data.Models
 {
     public class Error
     {
+        [IgnoreDataMember]
         public int Id { get; set; }
         public string? module { get; set; }
         public int ecode { get; set; }
@@ -15,20 +20,31 @@ namespace Data.Models
     }
     public class Files
     {
+        [IgnoreDataMember]
         public int Id { get; set; }
         public string? filename { get; set; }
         public bool result { get; set; }
-        public List<Error>? errors { get; set; }
+        public ICollection<Error>? errors { get ; set; }
         public DateTime scantime { get; set; }
+    }
+    public class FilesErrorsDTO
+    {
+        public string? filename { get; set; }
+        public ICollection<Error>? errors { get; set; }
+
+        public FilesErrorsDTO() { }
+        public FilesErrorsDTO(Files files) => (filename, errors) = (files.filename, files.errors);
     }
     public class Datas
     {
+        [IgnoreDataMember]
         public int Id { get; set; }
         public Scan? scan { get; set; }
-        public ICollection<Files>? files { get; set; } = new List<Files>();
+        public ICollection<Files>? files { get; set; }
     }
     public class Scan
     {
+        [IgnoreDataMember]
         public int Id { get; set; }
         public DateTime scanTime { get; set; }
         public string? db { get; set; }
@@ -41,5 +57,33 @@ namespace Data.Models
         public string? module { get; set; }
         public int ecode { get; set; }
         public string? error { get; set; }
+    }
+
+    public class FilenameCheckDTO
+    {
+        public int total { get; set; }
+        public int correct { get; set; }
+        public int errors_count { get; set; }
+        public string? filename { get; set; }
+        public string[]? filenames { get; set; }
+
+        public FilenameCheckDTO(Datas datas) => (total, correct, errors_count, filenames) = (
+            datas.files.Count(f => f.filename.Contains("query_")),
+            datas.files.Count(f => f.filename.Contains("query_") && f.result == true),
+            datas.files.Count(f => f.filename.Contains("query_") && f.result == false),
+            datas.files.Where(f => f.filename.Contains("query_") && f.result == false).Select(f => f.filename).ToArray()
+            );
+    }
+    public class ServiceInfoDto
+    {
+        static Assembly thisAssem = typeof(ServiceInfoDto).Assembly;
+        static AssemblyName thisAssemName = thisAssem.GetName();
+
+        static Version? ver = thisAssemName.Version;
+
+        public string? AppName { get { return thisAssemName.ToString(); } set { AppName = value; } }
+        public string? Version { get { return ver.ToString(); } set { Version = value; } }
+        public DateTime DateUtc { get { return DateTime.Now; } set { DateUtc = value; } }
+
     }
 }
--- minimal api/Program.cs
+++ minimal api/Program.cs
@@ -1,19 +1,39 @@
-using Data.Models;
+﻿using Data.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Linq;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DatasContext>(opt => opt.UseInMemoryDatabase("Datas"));
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
+Datas? dataJson = new Datas(); //поскольку требуется результаты считывания файла, нужна глобальная переменная
 
-app.MapGet("/api/allData", async (DatasContext db) =>
-    await db.Datas.ToListAsync());
+app.MapGet("/api/allData", (DatasContext db) =>
+{
+    return Results.Created($"/allData/{dataJson.Id}", dataJson);
+});
 
 app.MapGet("/api/scan", async (DatasContext db) =>
-    await db.Scan.ToListAsync());
+    await db.Scan.ToArrayAsync());
 
+//выбрасывает исключение при наличии вопросительного знака, но корректность все равно будет проверена
 app.MapGet("api/filenames/{value}", async (bool value, DatasContext db) =>
-    await db.Files.Where(r => r.result == false).ToListAsync());
+    await db.Files.Where(r => r.result == value).Select(f => f.filename).ToArrayAsync());
+
+app.MapGet("api/errors", (DatasContext db) =>
+{
+    var errorsDto = from b in db.Files
+                    where b.result == false
+                    select new FilesErrorsDTO(b)
+                    {
+                        filename = b.filename,
+                        errors = b.errors
+                    };
+    return errorsDto;
+});
+
 
 app.MapGet("api/errors/count", async (DatasContext db) =>
     await db.Scan.FindAsync(1)
@@ -21,19 +41,52 @@ app.MapGet("api/errors/count", async (DatasContext db) =>
             ? Results.Ok(scan.errorCount)
             : Results.NotFound());
 
-//app.MapGet("/api/filenames?correct={value}", async (bool value, AllDataContext db) =>
-//    await db.Files.FindAsync(value)
-//        is Files file
-//            ? Results.Ok(file)
-//            : Results.NotFound());
+app.MapGet("api/errors/{index}", (int index, DatasContext db) =>
+{ 
+    var errorsDto = from b in db.Files
+                    where b.result == false
+                    select new FilesErrorsDTO(b)
+                    {
+                        filename = b.filename,
+                        errors = b.errors
+                    };
+    if (index < errorsDto.ToList().Count)
+        return Results.Ok(errorsDto.ToList()[index]);
+    else
+        return Results.NotFound();
+});
+
+
+
+app.MapGet("api/query/check", (DatasContext db) =>
+{
+    var checkDto = from d in db.Datas
+                   select new FilenameCheckDTO(d)
+                   {
+                       total = d.files.Count(f => f.filename.Contains("query_")),
+                       correct = d.files.Count(f => f.filename.Contains("query_") && f.result == true),
+                       errors_count = d.files.Count(f => f.filename.Contains("query_") && f.result == false),
+                       filenames = d.files.Where(f => f.filename.Contains("query_") && f.result == false).Select(f => f.filename).ToArray()
+                   };
+    return checkDto;
+});
+
+app.MapGet("api/service/serviceinfo", (DatasContext db) =>
+{
+    return new ServiceInfoDto();
+});
+
 
 app.MapPost("/api/newErrors", async (HttpRequest request, DatasContext db) =>
 {
-    var data = await request.ReadFromJsonAsync<Datas>();
-    db.Datas.Add(data);
+    dataJson = await request.ReadFromJsonAsync<Datas>();
+    db.Datas.Add(dataJson);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/allData/{data.Id}", data);
+    if (dataJson == null)
+        return Results.NoContent();
+    else
+        return Results.Created($"/allData/{dataJson.Id}", dataJson);
 });
 
 app.Run();
